6c9b7284aa1aeae1dade2b5de0f257bfe5464017
Shawn Lunsford and Tanner Damron <shawn@>
2019-01-15 10:39:39 -0800

    add highlight js

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $("button#screen").click(function() {
     $("body").toggleClass("dark-background");
   });
+  $("h3#selecthighlight").click(function() {
+    $("h3#selecthighlight").toggleClass("highlight");
+  });
 });
